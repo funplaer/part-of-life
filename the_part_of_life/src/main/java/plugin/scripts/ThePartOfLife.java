798dfd16fda64151813d89039014ea7597aa44a1
funplaer <andrei.mizura55@gmail.com>
2026-05-29 12:12:00 +0300

    Поинт команда в отдельный скрипт в commands

--- the_part_of_life/src/main/java/plugin/scripts/ThePartOfLife.java
+++ the_part_of_life/src/main/java/plugin/scripts/ThePartOfLife.java
@@ -2,8 +2,9 @@ package plugin.scripts;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.scripts.commands.CommandsManager;
+import plugin.scripts.commands.PointsCommand;
 import plugin.scripts.core.GhostManager;
-import plugin.scripts.core.PointsManager;
+
 import plugin.scripts.core.ShowRewardTab;
 import plugin.scripts.listeners.DeathListener;
 import plugin.scripts.storage.StorageManager;
@@ -36,7 +37,7 @@ public class ThePartOfLife extends JavaPlugin {
         Objects.requireNonNull(getCommand("sethealth")).setExecutor(commandsManager);
         Objects.requireNonNull(getCommand("realkill")).setExecutor(commandsManager);
         Objects.requireNonNull(getCommand("respawn")).setExecutor(commandsManager);
-        Objects.requireNonNull(getCommand("showreward")).setExecutor(new PointsManager());
+        Objects.requireNonNull(getCommand("showreward")).setExecutor(new PointsCommand());
         Objects.requireNonNull(getCommand("showreward")).setTabCompleter(new ShowRewardTab());
     }
 
